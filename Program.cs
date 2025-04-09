79dda5f942c77c8b34ee4a043595d0896f628f3a
nguyensyhongquan <148837469+nguyensyhongquan@users.noreply.github.com>
2025-04-09 23:08:16 +0700

    Add view errror into StudentAdd.cshtml

--- Program.cs
+++ Program.cs
@@ -15,43 +15,44 @@ builder.Services.AddDbContext<FiliesContext>(options =>
 
 // Đăng ký IUserRepository và UserRepository vào DI container (nếu chưa đăng ký)
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Code thử nghiệm thêm user trong một scope
-using (var scope = app.Services.CreateScope())
-{
-    // Resolve IUserRepository
-    var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
 
-    // Tạo đối tượng User mới
-    var newUser = new User
-    {
-        Email = "nguyensyhongquan130703@gmail.com",
-        // Ban đầu, Passwordhash chứa mật khẩu dạng plain text
-        Passwordhash = "quanchin123",
-        Fullname = "Nguyen Sy Hong Quan",
-        AvatarUrl = "https://fliesenglish2025.blob.core.windows.net/avater/pngwing.com.png",
-        Role = "admin",
-        Balance = 0.00m,
-        Gender = "M",
-        Username = "quanvs2003",
-        Birthday = new DateTime(2003, 7, 13),
-        Address = "Nghe An",
-        PhoneNumber = "0343413939",
-        CreatedAt = DateTime.Now,
-        UpdatedAt = DateTime.Now,
-        Status = "active"
-    };
+//using (var scope = app.Services.CreateScope())
+//{
+//    // Resolve IUserRepository
+//    var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
 
-    // Gọi CreateUserAsync để thêm user, phương thức này sẽ tự hash mật khẩu bên trong
-    var createdUser = await userRepository.CreateUserAsync(newUser);
-    Console.WriteLine("dhwajbjjbjbjbjbjbjbjbjbjbjbjbjbb32111111111");
+//    // Tạo đối tượng User mới
+//    var newUser = new User
+//    {
+//        Email = "quanvs2003@gmail.com",
+//        // Ban đầu, Passwordhash chứa mật khẩu dạng plain text
+//        Passwordhash = "quanchin123",
+//        Fullname = "Nguyen Sy Hong Quan",
+//        AvatarUrl = "https://fliesenglish2025.blob.core.windows.net/avater/pngwing.com.png",
+//        Role = "student",
+//        Balance = 0.00m,
+//        Gender = "F",
+//        Username = "quanquan2003",
+//        Birthday = new DateTime(2003, 7, 10),
+//        Address = "Dong Nai",
+//        PhoneNumber = "0961459598",
+//        CreatedAt = DateTime.Now,
+//        UpdatedAt = DateTime.Now,
+//        Status = "active"
+//    };
 
-    Console.WriteLine($"User created with ID: {createdUser.UserId}");
-}
+//    // Gọi CreateUserAsync để thêm user, phương thức này sẽ tự hash mật khẩu bên trong
+//    var createdUser = await userRepository.CreateUserAsync(newUser);
+//    Console.WriteLine("dhwajbjjbjbjbjbjbjbjbjbjbjbjbjbb32111111111");
+
+//    //Console.WriteLine($"User created with ID: {createdUser.UserId}");
+//}
 
 // Phần cấu hình HTTP request pipeline của ứng dụng web
 if (!app.Environment.IsDevelopment())
@@ -66,9 +67,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Student}/{id?}");
 
 app.Run();
