8c1a0283b7acaea8c1af7c3bd025f1ee3990bacb
anhquanqh <quanqh12042000@gmail.com>
2025-04-09 16:53:12 +0700

    login,view profile

--- Controllers/Students/AccountController.cs
+++ Controllers/Students/AccountController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FliesProject.Data;
+using FliesProject.Repositories.IGenericRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FliesProject.Controllers.Students
 {
     public class AccountController : Controller
     {
+        private readonly IUserService _userService;
+        private readonly FiliesContext _dbContext;
+        public AccountController(FiliesContext dbContext, IUserService userService)
+        {
+            _userService = userService;
+            _dbContext = dbContext;  
+        }
         public IActionResult Home()
         {
             return View();
@@ -14,7 +24,30 @@ namespace FliesProject.Controllers.Students
         }
         public IActionResult ProfileStudent()
         {
-            return View();
+            var username = HttpContext.Session.GetString("UserName");
+
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("Home", "Account");
+            }
+
+            // Assuming _userService is a service that fetches user data
+            var user = _userService.GetUserByUsername(username);
+
+            if (user == null)
+            {
+                return RedirectToAction("Home", "Account");
+            }
+
+            _dbContext.Entry(user)
+                      .Collection(u => u.Courses)
+            .Load();
+
+            _dbContext.Entry(user)
+                      .Collection(u => u.EnrollementStudents)
+                      .Load();
+
+            return View(user);
         }
         [HttpPost]
         public IActionResult Logout()
